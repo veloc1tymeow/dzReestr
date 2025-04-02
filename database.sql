9d4de7b39a909f740877c58a1abc12ba6fdb7e86
veloc1tymeow <trashixx775@gmail.com>
2025-04-02 16:30:39 +0000

    Созданы таблицы home_animals и pack_animals

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 CREATE DATABASE human_friends;
 USE human_friends;
+CREATE TABLE home_animals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    type VARCHAR(50),
+    commands TEXT,
+    birth_date DATE
+);
+
+CREATE TABLE pack_animals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    type VARCHAR(50),
+    commands TEXT,
+    birth_date DATE
+);
